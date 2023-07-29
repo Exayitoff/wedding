0377a95553dfccda9704dea0b2805eae687a0b1a
qoshnazarov8317 <qoshnazarovsherali45@gmail.com>
2023-07-29 13:15:51 +0500

    create user model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='%. (class)s_created_by', null=True, blank=True)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='%(class)s_updated_by', on_delete=models.SET_NULL, null=True, blank=True)
+    
+    class Meta:
+        abstract=True
--- users/admin.py
+++ users/admin.py
@@ -1,19 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .forms import UserCreationForm, UserChangeForm
 from .models import User
 
-
-class UserAdmin(UserAdmin):
-    add_form = UserCreationForm
-    form = UserChangeForm
-    model = User
-    list_display = ['full_name', 'phone_number', 'is_phone_confirmed', 'image', 'role', 'is_active']
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('full_name', 'phone_number', 'is_phone_confirmed', 'image', 'role', )}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('full_name', 'phone_number', 'is_phone_confirmed', 'image', 'role', )}),
-    )
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
