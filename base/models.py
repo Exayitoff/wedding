30b782e4551401f00438c8eae330e17c3aea9644
qoshnazarov8317 <qoshnazarovsherali45@gmail.com>
2023-08-01 10:14:34 +0500

    Service

--- base/models.py
+++ base/models.py
@@ -4,8 +4,8 @@ from django.conf import settings
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     # created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='%. (class)s_created_by', null=True, blank=True)
     # updated_by = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='%(class)s_updated_by', on_delete=models.SET_NULL, null=True, blank=True)
     
