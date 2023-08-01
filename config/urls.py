a9178df1e7500609c6f4012e9a9b5c21174841aa
RustamovBekzod <bekzodrustamov1425@gmail.com>
2023-08-01 12:29:04 +0500

    'swagger'

--- config/urls.py
+++ config/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .yasg import urlpatterns as doc_api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('service.urls')),
 ]
+urlpatterns += doc_api
 
 
 if settings.DEBUG:
