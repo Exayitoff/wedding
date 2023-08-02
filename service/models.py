b67f045070deef976ec37fbcec1a344068600f28
RustamovBekzod <bekzodrustamov1425@gmail.com>
2023-08-02 16:20:41 +0500

    service

--- service/models.py
+++ service/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from base.models import BaseModel
+
+from phonenumber_field.modelfields import PhoneNumberField
+from base.models import Region, District
+from users.models import User
+
 # Create your models here.
 
 class ServiceType(BaseModel):
@@ -7,3 +12,21 @@ class ServiceType(BaseModel):
     
     def __str__(self):
         return f'{self.name}'
+    
+    
+class Service(BaseModel):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.IntegerField()
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    address = models.CharField(max_length=250, blank=True, null=True)
+    phone_number = PhoneNumberField(blank=False, null=False)
+    additional_phone_number = PhoneNumberField(blank=True, null=True)
+    telegram = models.CharField(max_length=250, blank=True, null=True)
+    instagram = models.CharField(max_length=250, blank=True, null=True)
+    youtube = models.CharField(max_length=250, blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
--- service/urls.py
+++ service/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ServiceTypeList
+from .views import ServiceTypeList, ServiceList
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('service-types', ServiceTypeList, basename='service')
+router.register('service-types', ServiceTypeList, basename='service-types')
+router.register('services', ServiceList, basename='services')
 urlpatterns = router.urls   
