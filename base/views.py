00e6fda229dececd8e4b39b7d2853db20c7ee3aa
Sherali <122153818+qoshnazarov8317@users.noreply.github.com>
2023-08-04 11:29:36 +0500

    Added search and filter (#13)
    * Added search and filter
    
    * remove migration files

--- base/views.py
+++ base/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from .serializers import RegionSerializer, DistrictSerializer
 from .models import Region, District
 
@@ -7,7 +9,13 @@ from .models import Region, District
 class RegionList(viewsets.ModelViewSet):
     queryset = Region.objects.all()
     serializer_class = RegionSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['name']
+    search_fields = ['name']
     
 class DistrictList(viewsets.ModelViewSet):
     queryset = District.objects.all()
     serializer_class = DistrictSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['region', 'name']
+    search_fields = ['region__name', 'name']
